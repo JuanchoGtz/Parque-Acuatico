22c8f5174f3b1ff26d21f965fd85238ccc98ccc3
Juancho Gutierrez <gutierrezgonzalezjuan05@gmail.com>
2025-04-01 22:53:38 -0600

    Se crearon las rutas en el archivo Routes.php, la conexion la base de datos se encuentra en dos archivos env y en app\config\Database.php

--- parque-acuatico-api/app/Config/Routes.php
+++ parque-acuatico-api/app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->post('login', 'AuthController::login');
+
+$routes->get('admin/usuarios', 'AdminController::usuarios');
+$routes->get('admin/productos', 'AdminController::productos');
+$routes->get('admin/ventas', 'AdminController::ventas');
+
+$routes->post('cliente/comprar', 'ClienteController::comprar');
